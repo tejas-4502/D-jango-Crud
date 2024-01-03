9ad968107e8b1f187c304b2b5273978ef3ff895b
Tejas <tejassonawane171@gmail.com>
2024-01-03 22:49:35 +0530

    first

---
+++ test_django/crud/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('display/', views.crud, name='display'),
+    path('insert/', views.insert_view, name='insert'),
+    path('edit/<int:id>/', views.edit_view, name='edit'),  
+    path('delete/<int:id>/', views.delete_view, name='delete'), 
+]
---
+++ test_django/crud/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Crud
+from django.http import HttpResponseRedirect
+
+# Existing display function
+def crud(request):
+    crud_data = Crud.objects.all().values()
+    return render(request, 'display.html', {'crud': crud_data})
+
+# Adding function
+def insert_view(request):
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        Crud.objects.create(firstname=firstname, lastname=lastname)
+        return HttpResponseRedirect('/display/')  # Redirect to display page
+    return render(request, 'create.html')
+
+
+
+def edit_view(request, id):
+    instance = get_object_or_404(Crud, id=id)
+    if request.method == 'POST':
+        # Update the record with new data
+        instance.firstname = request.POST.get('firstname')
+        instance.lastname = request.POST.get('lastname')
+        instance.save()
+        return HttpResponseRedirect('/display/')  # Redirect to display page
+    return render(request, 'edit.html', {'instance': instance})
+
+# Delete function
+def delete_view(request, id):
+    instance = get_object_or_404(Crud, id=id)
+    instance.delete()
+    return HttpResponseRedirect('/display/')  # Redirect to display page
