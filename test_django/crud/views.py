cbf53366e86e5e96d22e10b7202adc244d0ba5f3
Tejas <tejassonawane171@gmail.com>
2024-01-08 14:24:55 +0530

    messages feedback

--- test_django/crud/views.py
+++ test_django/crud/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Crud
 from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.core.paginator import Paginator
 
 def home(request):
     return render(request, 'home.html')
 
-# Existing display function
+# display function
 def crud(request):
-    crud_data = Crud.objects.all().values()
-    return render(request, 'display.html', {'crud': crud_data})
+    crud_data = Crud.objects.all()
+    paginator = Paginator(crud_data, 5)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'display.html', {'page_obj': page_obj})
 
 # Adding function
 def insert_view(request):
@@ -18,15 +25,15 @@ def insert_view(request):
         mobno = request.POST.get('mobno')
         email = request.POST.get('email')
         Crud.objects.create(firstname=firstname, lastname=lastname, mobno=mobno, email=email)
+        messages.success(request, 'User created successfully!')
         return HttpResponseRedirect('/display/')  # Redirect to display page
     return render(request, 'create.html')
 
-
-
+# Update function
 def edit_view(request, id):
     instance = get_object_or_404(Crud, id=id)
     if request.method == 'POST':
-        # Update the record with new data
+        # Update the data
         instance.firstname = request.POST.get('firstname')
         instance.lastname = request.POST.get('lastname')
         instance.mobno = request.POST.get('mobno')
